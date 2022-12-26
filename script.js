887cd17bc54ea94c35521b4e3ad377853f772edc
jpca <jpca123555@gmail.com>
2022-12-26 00:37:34 -0500

    Arreglando estilos

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ navegacion.addEventListener('click', e=>{
 	if(e.target.matches('.navegacion-link')) btnMenu.checked = false;
 })
 
+//scrollSpy
+let sections = Array.from(document.querySelectorAll("section[data-scroll-spy]"));
+
+let options = {threshold: [0.5]}
+
+let scrollSpy = new IntersectionObserver((entries)=>{
+
+	entries.forEach(entry =>{
+			// let activos = navegacion.querySelectorAll(".section-active");
+			// if (activos) activos.forEach(el => el.classList.remove("section-active"));
+		if (entry.isIntersecting){
+			let id = `#${entry.target.id}`
+			console.log(id)
+			navegacion.querySelector(`a[href="#${entry.target.id}"]`).classList.add("navegacion-link-active");
+		}else{
+			navegacion.querySelector(`a[href="#${entry.target.id}"]`).classList.remove("navegacion-link-active");
+		}
+	})
+
+
+
+	// Array.from(document.querySelectorAll(".section-active")).forEach(el => el.classList.remove("section-active"));
+
+	// navegacion.children[indexEntry].classList.add("section-active")
+}, options);
+
+document.querySelectorAll("section[data-scroll-spy]").forEach(el => scrollSpy.observe(el));
